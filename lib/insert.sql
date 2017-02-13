70df398a6247d70a3ed149d9ec6df05c2fafd11d
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-13 01:58:25 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 32, "M", "Brown", "Sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "M", "Brown", "Rowdy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Tan", "Childlike", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 40, "M", "Blue", "Blue Lives Matter", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 55, "F", "White", "Exploitative", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 60, "M", "Green", "Miserly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 15, "F", "Panda", "Standard panda temperament", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 28, "M", "Brown", "HUNGRY", 0);
